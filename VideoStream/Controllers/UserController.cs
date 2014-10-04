c17d33f0aabb06f869f17a40551e5ac65b881c52
Denethrillien <just-alex@live.no>
2014-10-04 12:20:46 +0200

    Initial framework

---
+++ VideoStream/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoStream.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.UserModel user)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Models.UserModel user)
+        {
+            return View();
+        }
+
+        private bool isValid(string eMail, string password)
+        {
+            var crypto = new SimpleCrypto.PBKDF2();
+            bool isValid = false;
+
+            using(var db = new DataEntities())
+            {
+                var user = db.Users.FirstOrDefault(u => u.E_mail == eMail);
+                //If the user exists,
+                if (user != null) 
+                { 
+                    //continue validating.
+                    if (user.Password == crypto.Compute(password, user.PasswordSalt))
+                    { 
+                        //Validated
+                        isValid = true;
+                    }
+                }
+            }
+            return isValid;
+        }
+    }
+}
---
+++ VideoStream/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VideoStream.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength=8)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(20, MinimumLength=3)]
+        public string UserName { get; set; }
+    }
+}
