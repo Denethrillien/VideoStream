1411a14268e7ae5657257856379d957f2723bf55
Denethrillien <just-alex@live.no>
2014-10-05 10:59:02 +0200

    Added timers to GUI elements

--- MediaPlayer/MainPage.xaml.cs
+++ MediaPlayer/MainPage.xaml.cs
@@ -9,11 +9,16 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MediaPlayer
 {
     public partial class MainPage : UserControl
     {
+        DispatcherTimer _timer = new DispatcherTimer();
+        int counter = 0;
+        int _interval_time = 2;              
+
         public MainPage()
         {
             InitializeComponent();
@@ -50,25 +55,57 @@ namespace MediaPlayer
         }
 
         //Hidecontrols
-        private void Controls_MouseLeave(object sender, MouseEventArgs e)
+        private void hideControls(object sender, MouseEventArgs e)
         {
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (s, t) =>
+            {
+                counter++;
 
+                if (counter >= _interval_time)
+                {
+                    _timer.Stop();
+
+                    controls.Visibility = System.Windows.Visibility.Collapsed;
+                    volSlider.Visibility = System.Windows.Visibility.Collapsed;
+                    currentMedia.Visibility = System.Windows.Visibility.Collapsed;
+                    bottomBar.Visibility = System.Windows.Visibility.Visible;
+                }
+
+            };
+            _timer.Start();
         }
 
         //Showcontrols
-        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        private void showControls(object sender, MouseEventArgs e)
         {
-
+            controls.Visibility = System.Windows.Visibility.Visible;
+            currentMedia.Visibility = System.Windows.Visibility.Visible;
+            bottomBar.Visibility = System.Windows.Visibility.Collapsed;
         }
 
         private void showVol(object sender, MouseEventArgs e)
         {
-
+            volSlider.Visibility = System.Windows.Visibility.Visible;
         }
 
         private void hideVol(object sender, MouseEventArgs e)
         {
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (s, t) =>
+            {
+                counter++;
 
+                if (counter >= _interval_time)
+                {
+                    _timer.Stop();
+
+                    volSlider.Visibility = System.Windows.Visibility.Collapsed;
+                }
+
+            };
+            _timer.Start();
         }
+
     }
 }
